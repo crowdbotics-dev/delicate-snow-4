092a4a3cbd7db0b7da7b0ec6e58b44141ef9934b
Crowdbotics <engineering@crowdbotics.com>
2024-04-09 19:59:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-snow-4",
+  url: "https://delicate-snow-4.botics.co",
+  api: "https://delicate-snow-4.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
